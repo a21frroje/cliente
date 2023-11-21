ab3e2a67713d4e9dd82013736adec606b9da8334
Jesus Frias <a21frroje@iesgrancapitan.org>
2023-11-21 19:31:06 +0100

    terminado

--- main.js
+++ main.js
@@ -7,31 +7,48 @@ document.addEventListener("DOMContentLoaded",function(){
     let Decimal = document.getElementById("decimal");
     let Hexadecimal = document.getElementById("hexadecimal");
 
+    function octDecimal(number) {
+        return parseInt(number, 8)
+    }
+    function decimalHex(number) {
+        return number.toString(16)
+    }
+    function decimalOct(number) {
+        return number.toString(8)
+    }
+    function decimalBin(number) {
+        return number.toString(2)
+    }
+
     function compruebaBinario(number) {
-        // Verificar si la entrada es un número binario válido
-        if (/^[01]+$/.test(number)) {
-            // Convertir el número binario a decimal usando parseInt
-            var all = Array()
-            all.push(parseInt(number, 2));
-            all.push(parseInt(number, 8));
-            all.push(parseInt(number, 16));
-            return all;
-        } else {
-            return "Por favor, introduce un número binario válido.";
-        }
+        var all = Array()
+        all.push(parseInt(number, 2));
+        all.push(decimalOct(all[0]));
+        all.push(decimalHex(all[0]));
+        return all;
+
     }
     function compruebaOctal(number) {
-        // Verificar si la entrada es un número binario válido
-        if (/^[012345678]+$/.test(number)) {
-            // Convertir el número binario a decimal usando parseInt
-            var all = Array()
-            all.push(parseInt(number, 1));
-            all.push(parseInt(number, 2));
-            all.push(parseInt(number, 16));
-            return all;
-        } else {
-            return "Por favor, introduce un número binario válido.";
-        }
+        var all = Array()
+        all.push(octDecimal(number));
+        all.push(decimalHex(all[0]));
+        all.push(decimalBin(all[0]));
+        return all;
+    }
+    function compruebaDecimal(number) {
+        var all = Array();
+        number = parseInt(number);
+        all.push(decimalBin(number));
+        all.push(decimalHex(number));
+        all.push(decimalOct(number));
+        return all;
+    }
+    function compruebaHexa(number) {
+        var all = Array();
+        all.push(parseInt(number, 16));
+        all.push(decimalBin(all[0]));
+        all.push(decimalOct(all[0]));
+        return all;
     }
     Binario.addEventListener("blur", function() {
         document.getElementById("decimal").value = compruebaBinario(Binario.value)[0];
@@ -39,8 +56,18 @@ document.addEventListener("DOMContentLoaded",function(){
         document.getElementById("hexadecimal").value = compruebaBinario(Binario.value)[2];
     })
     Octal.addEventListener("blur", function() {
-        document.getElementById("binario").value = compruebaOctal(Octal.value)[0];
-        document.getElementById("decimal").value = compruebaOctal(Octal.value)[1];
-        document.getElementById("hexadecimal").value = compruebaOctal(Octal.value)[2];
+        document.getElementById("decimal").value = compruebaOctal(Octal.value)[0];
+        document.getElementById("hexadecimal").value = compruebaOctal(Octal.value)[1];
+        document.getElementById("binario").value = compruebaOctal(Octal.value)[2];
+    })
+    Decimal.addEventListener("blur", function() {
+        document.getElementById("binario").value = compruebaDecimal(Decimal.value)[0];
+        document.getElementById("hexadecimal").value = compruebaDecimal(Decimal.value)[1];
+        document.getElementById("octal").value = compruebaDecimal(Decimal.value)[2];
+    })
+    Hexadecimal.addEventListener("blur", function() {
+        document.getElementById("decimal").value = compruebaHexa(Hexadecimal.value)[0];
+        document.getElementById("binario").value = compruebaHexa(Hexadecimal.value)[1];
+        document.getElementById("octal").value = compruebaHexa(Hexadecimal.value)[2];
     })
 })
